a627591b10ff12a5274c3007ffd9294ac119587c
Vinicius <pedro.braz@ccc.ufcg.edu.br>
2025-04-05 20:18:11 -0300

    Rota de cadastro completa e algumas alterações no pom

--- src/main/java/com/example/demo/dto/ProductSaveDTO.java
+++ src/main/java/com/example/demo/dto/ProductSaveDTO.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -11,7 +11,7 @@ import lombok.Setter;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
+public class ProductDTO {
     private Long id;
     private String name;
     private Double price;
